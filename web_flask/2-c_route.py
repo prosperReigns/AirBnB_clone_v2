399a88950799cafad9ba9f4047fdd4aa5dd82b5f
prosperReigns <prosperkinging7@gmail.com>
2024-05-31 11:33:27 -0700

    task 1 and 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,5 +11,17 @@ def hello():
     return 'Hello HBNB!'
 
 
+@app.route("/hbnb")
+def about():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def show_text(text):
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"C {text}"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
